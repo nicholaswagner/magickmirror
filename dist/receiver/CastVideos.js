78e8cde1fe4b8b26101adbc840a2b0a42cb6a75d
nicholaswagner <nicholas@secretsignal.com>
2019-12-22 07:23:44 -0600

    adds app id to castvideos.js

--- dist/receiver/CastVideos.js
+++ dist/receiver/CastVideos.js
@@ -168,7 +168,8 @@ CastPlayer.prototype.initializeCastPlayer = function () {
   // Set the receiver application ID to your own (created in the
   // Google Cast Developer Console), or optionally
   // use the chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID
-  options.receiverApplicationId = 'C0868879';
+  //options.receiverApplicationId = 'C0868879';
+  options.receiverApplicationId = '3B2AFFAA';
 
   // Auto join policy can be one of the following three:
   // ORIGIN_SCOPED - Auto connect from same appId and page origin
