28bf1d2abe9bc52e49a4de884a60d5caae3847c7
nicholaswagner <nicholas@secretsignal.com>
2019-12-22 15:17:35 -0600

    fixes more example bugs

--- dist/cast/CastVideos.js
+++ dist/cast/CastVideos.js
@@ -937,7 +937,7 @@ var mediaJSON = {
                 'description': "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
                 'sources': ['https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'],
                 'subtitle': 'By Blender Foundation',
-                'thumb': 'images/BigBuckBunny.jpg',
+                'thumb': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg',
                 'title': 'Big Buck Bunny'
             }
         ]
