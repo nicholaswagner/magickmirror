039b456ab7f8088245a6594eac7f3c378bad4c03
nicholaswagner <nicholas@secretsignal.com>
2019-12-22 15:13:27 -0600

    fixes more https issues

--- dist/cast/CastVideos.js
+++ dist/cast/CastVideos.js
@@ -935,7 +935,7 @@ var mediaJSON = {
         'videos': [
             {
                 'description': "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-                'sources': ['//commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'],
+                'sources': ['https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'],
                 'subtitle': 'By Blender Foundation',
                 'thumb': 'images/BigBuckBunny.jpg',
                 'title': 'Big Buck Bunny'
